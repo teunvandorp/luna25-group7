14f3dd8992f36677ce5481385f68a0aba2dcab06
Teun van Dorp <55231254+teunvandorp@users.noreply.github.com>
2026-04-21 23:15:11 +0200

    test config

--- experiment_config.py
+++ experiment_config.py
@@ -5,7 +5,7 @@ class Configuration(object):
     def __init__(self) -> None:
 
         # Working directory
-        self.WORKDIR = Path("C:/temp/luna25-baseline")
+        self.WORKDIR = Path("/vol/csedu-nobackup/course/IMC037_aimi/group07")
         self.RESOURCES = self.WORKDIR / "resources"
         # Starting weights for the I3D model
         self.MODEL_RGB_I3D = (
@@ -14,12 +14,12 @@ class Configuration(object):
         
         # Data parameters
         # Path to the nodule blocks folder provided for the LUNA25 training data. 
-        self.DATADIR = Path("V:/projects/luna25/NLST/nodule_blocks")
+        self.DATADIR = Path("/vol/csedu-nobackup/course/IMC037_aimi/group07/data/luna25_nodule_blocks")
         # Path to the folder containing the CSVs for training and validation.
-        self.CSV_DIR = Path("V:/projects/luna25/dataset_csv")
+        self.CSV_DIR = Path("/vol/csedu-nobackup/course/IMC037_aimi/group07/data")
         # We provide an NLST dataset CSV, but participants are responsible for splitting the data into training and validation sets.
-        self.CSV_DIR_TRAIN = self.CSV_DIR / "train.csv" # Path to the training CSV
-        self.CSV_DIR_VALID = self.CSV_DIR / "valid.csv" # Path to the validation CSV
+        self.CSV_DIR_TRAIN = self.CSV_DIR / "LUNA25_Public_Training_Development_Data.csv" # Path to the training CSV
+        self.CSV_DIR_VALID = self.CSV_DIR / "LUNA25_Public_Training_Development_Data.csv" # Path to the validation CSV
 
         # Results will be saved in the /results/ directory, inside a subfolder named according to the specified EXPERIMENT_NAME and MODE.
         self.EXPERIMENT_DIR = self.WORKDIR / "results"
@@ -27,7 +27,7 @@ class Configuration(object):
             self.EXPERIMENT_DIR.mkdir(parents=True)
             
         self.EXPERIMENT_NAME = "LUNA25-baseline"
-        self.MODE = "2D" # 2D or 3D
+        self.MODE = "3D" # 2D or 3D
 
         # Training parameters
         self.SEED = 2025
@@ -37,7 +37,7 @@ class Configuration(object):
         self.BATCH_SIZE = 32
         self.ROTATION = ((-20, 20), (-20, 20), (-20, 20))
         self.TRANSLATION = True
-        self.EPOCHS = 10
+        self.EPOCHS = 2
         self.PATIENCE = 20
         self.PATCH_SIZE = [64, 128, 128]
         self.LEARNING_RATE = 1e-4
